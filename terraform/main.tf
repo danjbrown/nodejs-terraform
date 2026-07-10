5d7bbf0af40952b08e458a7513f9ea9234b25c22
Dan Brown <dbrown.email@gmail.com>
2026-07-10 17:28:33 +0100

    feat(nodejs-terraform-app): update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "dbrown-terraform-state-bucket"
     key    = "ecs/nodejs-terraform-app/terraform.tfstate"
-    region = "ap-south-1"
+    region = "eu-west-2"
   }
 }
 
